f03d00cccae7db223fcc9c32b814e5de43ef9e55
Connor Scott <141468326+Connor-Scott@users.noreply.github.com>
2024-04-20 14:51:29 -0700

    Add files via upload

--- app/graph.py
+++ app/graph.py
@@ -1,27 +1,35 @@
+
 import altair as alt
-import numpy as np
 
 
-# def damage_mean(damage, samples):
-#     parts = damage.split('d')
-#     rolls = int(parts[0])
-#
-#     if '+' in parts[1]:
-#         die, modifier = int(parts[1].split('+')[0]), int(parts[1].split('+')[1])
-#     else:
-#         die, modifier = int(parts[1]), 0
-#
-#     attacks = np.random.randint(1, die + 1, (samples, rolls)).sum(axis=1) + modifier
-#     mean = np.mean(attacks)
-#
-#     return mean
-
-
-def Chart(df, x, y, target):
-    chart = alt.Chart(df, title=f"{x} by {y} and {target}").mark_point().encode(
+def chart(df, x, y, target):
+    graph = alt.Chart(df, title=f"{x} by {y} and {target}").mark_point().encode(
         y=y,
         x=x,
         color=alt.Color(target, scale=alt.Scale(scheme='lightgreyred'))
-    )
-    chart = chart.configure_view(fill='#252525')
-    return chart.to_json()
+    ).properties(
+        width=800,
+        height=650,
+        background="#252525",
+    ).configure(
+        axis={
+            "titlePadding": 10,
+            "labelPadding": 5,
+            "gridColor": "Black",
+            "tickColor": "Black",
+            "titleColor": "#AAAAAA",
+            "labelColor": "#AAAAAA",
+            "domainColor": "Black",
+            },
+        legend={
+            "titleColor": "#AAAAAA",
+            "labelColor": "#AAAAAA",
+            },
+        title={
+            "color": "#AAAAAA",
+            "fontSize": 25,
+            },
+        )
+    return graph
+
+
